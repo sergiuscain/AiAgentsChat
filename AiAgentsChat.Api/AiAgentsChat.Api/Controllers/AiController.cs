35beb079aa42c72fe9f9f12b6b1f7f19bed27eb6
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2026-01-06 14:52:40 +0300

    Рефакторинг: привел указания пространства имён к общему стилю решения.

--- AiAgentsChat.Api/AiAgentsChat.Api/Controllers/AiController.cs
+++ AiAgentsChat.Api/AiAgentsChat.Api/Controllers/AiController.cs
@@ -1,30 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AiAgentServiceLib;
 
-namespace AiAgentsChat.Api.Controllers
+namespace AiAgentsChat.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AiController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AiController : ControllerBase
+    private AiAgentsFabric _aiAgentsFabric;
+    public AiController(AiAgentsFabric aiAgentsFabric)
     {
-        private AiAgentsFabric _aiAgentsFabric;
-        public AiController(AiAgentsFabric aiAgentsFabric)
-        {
-            _aiAgentsFabric = aiAgentsFabric;
-        }
-        [HttpPost("PostApiPrompt")]
-        public async Task<IActionResult> PostApiPromptAsync(string prompt, string AiAgentName)
-        {
+        _aiAgentsFabric = aiAgentsFabric;
+    }
+    [HttpPost("PostApiPrompt")]
+    public async Task<IActionResult> PostApiPromptAsync(string prompt, string AiAgentName)
+    {
            
-           var result = await _aiAgentsFabric.PostPromptAsync(prompt, AiAgentName);
-            if (result != null) 
-                return Ok(result);
-            return BadRequest("Ошибка отправке запроса");
-        }
-        [HttpPost("CreateAiAgent")]
-        public bool CreateAgent(string AiAgentName)
-        {
-            return _aiAgentsFabric.CreateAgent(AiAgentName);
-        }
+        var result = await _aiAgentsFabric.PostPromptAsync(prompt, AiAgentName);
+        if (result != null) 
+            return Ok(result);
+        return BadRequest("Ошибка отправке запроса");
+    }
+    [HttpPost("CreateAiAgent")]
+    public bool CreateAgent(string AiAgentName)
+    {
+        return _aiAgentsFabric.CreateAgent(AiAgentName);
     }
 }
